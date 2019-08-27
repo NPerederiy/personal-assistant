dd8cd8f60bd120966adbb4d7d0b9de74dc085047
NPerederiy <nick.perederiy@gmail.com>
2019-08-27 15:39:51 +0300

    J-0013 Implement currency service

--- FinancialControl.BL/Services/Abstractions/ICurrencyService.cs
+++ FinancialControl.BL/Services/Abstractions/ICurrencyService.cs
@@ -1,10 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using FinancialControl.BL.BO;
+using System.Threading.Tasks;
 
 namespace FinancialControl.BL.Services.Abstractions
 {
     public interface ICurrencyService
     {
-        Task GetCurrencyInfo(string currencyCode);
-        Task AddCurrencyAsync();
+        Task<CurrencyBO> GetCurrencyInfo(string ISO_4217_Code);
+        Task<CurrencyBO> AddCurrencyAsync(string ISO_4217_Code, string ISO_4217_Number, string name);
     }
 }
---
+++ FinancialControl.BL/Services/Implementations/CurrencyService.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using FinancialControl.BL.BO;
+using FinancialControl.BL.Services.Abstractions;
+using FinancialControl.DAL.Entities;
+using FinancialControl.DAL.Repositories.Abstractions;
+using System.Threading.Tasks;
+
+namespace FinancialControl.BL.Services.Implementations
+{
+    public class CurrencyService : ICurrencyService
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IMapper _mapper;
+
+        public CurrencyService(
+            IUnitOfWork uow, 
+            IMapper mapper
+        )
+        {
+            _uow = uow;
+            _mapper = mapper;
+        }
+
+        public async Task<CurrencyBO> AddCurrencyAsync(string ISO_4217_Code, string ISO_4217_Number, string name)
+        {
+            var currency = new Currency
+            {
+                ISO_4217_Code = ISO_4217_Code,
+                ISO_4217_Number = ISO_4217_Number,
+                Name = name
+            };
+
+            await _uow.CurrencyRepository.CreateAsync(currency);
+            return _mapper.Map<CurrencyBO>(currency);
+        }
+
+        public async Task<CurrencyBO> GetCurrencyInfo(string ISO_4217_Code)
+        {
+            var currency = await _uow.CurrencyRepository.GetByConditionAsync(x => x.ISO_4217_Code == ISO_4217_Code);
+            return currency != null ? _mapper.Map<CurrencyBO>(currency) : null;
+        }
+    }
+}
