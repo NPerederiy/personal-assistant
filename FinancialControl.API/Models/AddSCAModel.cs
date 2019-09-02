46604c1541c4528a0efe95ee69e6afeb4cc73fa8
NPerederiy <nick.perederiy@gmail.com>
2019-09-02 11:07:11 +0300

    J-0025 Add account controller

---
+++ FinancialControl.API/Models/AddSCAModel.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinancialControl.API.Models
+{
+    public class AddSCAModel
+    {
+        [Required]
+        [JsonProperty("ownerId")]
+        public string OwnerId { get; set; }
+
+        [Required]
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [Required]
+        [JsonProperty("currencyCode")]
+        public string CurrencyCode { get; set; }
+
+        [JsonProperty("cardNumber")]
+        public string AccountNumber { get; set; }
+
+        [JsonProperty("cardValidTo")]
+        public string ValidTo { get; set; }
+
+        [JsonProperty("cardType")]
+        public string BankCardType { get; set; }
+    }
+}
---
+++ FinancialControl.API/Models/RenameAccountModel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinancialControl.API.Models
+{
+    public class RenameAccountModel
+    {
+        [Required]
+        [JsonProperty("accountId")]
+        public string AccountId { get; set; }
+
+        [Required]
+        [JsonProperty("name")]
+        public string NewName { get; set; }
+    }
+}
