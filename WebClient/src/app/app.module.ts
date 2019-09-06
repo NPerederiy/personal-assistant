e956c0b9558f021706e43468f9f90e9067966b9a
NPerederiy <nick.perederiy@gmail.com>
2019-09-06 18:37:30 +0300

    J-0030 Add ngx-translate

--- WebClient/src/app/app.module.ts
+++ WebClient/src/app/app.module.ts
@@ -16,6 +16,9 @@ import { IncomeControlPageComponent } from './income-control-page/income-control
 import { CostControlPageComponent } from './cost-control-page/cost-control-page.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -35,9 +38,22 @@ import { appRoutes } from './app.routes';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+  })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
