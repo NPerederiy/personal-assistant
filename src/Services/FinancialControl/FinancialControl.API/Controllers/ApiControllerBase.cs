9cf202fc19cd5b7db415fff1319ae35f967c53e9
NPerederiy <nick.perederiy@gmail.com>
2021-07-08 15:46:12 +0300

    J-0046 Started porting the backend

---
+++ src/Services/FinancialControl/FinancialControl.API/Controllers/ApiControllerBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinancialControl.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiControllerBase : ControllerBase
+    {
+        
+    }
+}
